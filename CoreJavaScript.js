23e3cbc4983e90120ab55c04600c797c7bfd7655
bobsira <sbobfitz2@gmail.com>
2017-09-18 13:56:28 +0300

    Updates on the files with javascript expressions overview

--- CoreJavaScript.js
+++ CoreJavaScript.js
@@ -44,3 +44,32 @@ primes[4] = 9;                // Add a new element by assignment.
 primes[4] = 11;               // Or alter an existing element by assignment.
 var empty = [];               // [] is an empty array with no elements.
 empty.length                  // => 0
+
+
+// Arrays and objects can hold other arrays and objects:
+var points = [                      // An array with 2 elements.
+  {x:0, y:0},                       // Each element is an object.
+  {x:1, y:1}
+];
+
+var data = {                        // An object with 2 properties
+  trial1: [ [1,2], [3,4] ],         // The value of each property is an array
+  trial2: [ [2,3], [4,5] ]          // The elements of the arrays are arrays.
+};
+
+// Operators act on values (the operands) to produce a new value.
+// Arithmetic operators are the most common:
+3 + 2                     // =>5: addition
+3 - 2                     // =>1: subtraction
+3 * 2                     // =>6: multiplication
+3 / 2                     // =>1.5 division
+points[1].x - points[0].x // =>1; more complicated operands work, too
+"3" + "2"                 // => "32" + adds numbers, concatenates strings
+
+// JavaScript defines some shorthand arithmetic operators
+var count = 0;            // Define a variable
+count++;                  // Increment the variable
+count--;                  // Decrement the variable
+count += 2;               // Add 2: same as count = count + 2;
+count *= 3;               // Multiply by 3: same as count = count * 3
+count                     // =>: variable names are expressions, too.
