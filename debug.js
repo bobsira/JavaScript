fa3b932fc44cc2151701f6ff74dfe0fd7aeb4b49
bobsira <sbobfitz2@gmail.com>
2017-09-25 13:53:30 +0300

    jQuery code example

--- debug.js
+++ debug.js
@@ -18,3 +18,13 @@ function debug(msg) {
   pre.appendChild(text);                   // Add text to the <pre>
   log.appendChild(pre);                    // Add <pre> to the log
 }
+
+// // jQuery
+// function debug(msg) {
+//   var log = $("#debuglog";);     //Find the element to display msg in.
+//   if (log.length==0) {           //If it doesn't exist yet, create it...
+//     log = $("<div id='debuglog'></h1>Debug Log</h1></div>");
+//     log.appendTo(document.body); //and insert it at the end of the body.
+//   }
+//   log.append($("<pre/>").text(msg)); // Wrap msg in <pre> and append to log.
+// }
