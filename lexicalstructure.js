4e76795f1a569f24132c705e47f53d56b7a54f35
bobsira <sbobfitz2@gmail.com>
2017-10-03 07:57:15 +0300

    Literals

--- lexicalstructure.js
+++ lexicalstructure.js
@@ -4,3 +4,16 @@
 * This is yet another comment.
 * It has multiple lines.
 */
+
+// A literal is a data value that appears directly in a program
+12            // The number twelve
+1.2           // The number one point two
+"hello world" // A string of text
+'Hi'          // Another string
+true          // A Boolean value
+false         // The other Boolean value
+/javascript/git // A "regular expression" literal (for pattern matching)
+null            // Absence of an object
+
+{x:1, y:2}     // An object initializer
+[1,2,3,4,5]    // An array initializer
