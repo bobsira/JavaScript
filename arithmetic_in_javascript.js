db4bde418fcc3335591ebef928f569108b9aedee
bobsira <sbobfitz2@gmail.com>
2017-10-24 22:53:13 +0300

    Arithmetic in JavaScript

---
+++ arithmetic_in_javascript.js
@@ -0,0 +1,16 @@
+include +,-,*,/,%
+
+other mathematical operations
+Math.pow(2,53);  // 2 to the power of 53
+Math.round(.6);  // 1.0 round to the nearest int
+Math.ceil(.6);   // 1.0 round up to an int
+Math.floor(.6);  // 0.0 round down to an int
+Math.abs(-5);    // 5
+Math.max(1,7,3); // 7
+Math.min(4,2,9); // 2
+Math.random();   // Pseudo-random number x where 0<=x <1.0
+Math.PI;
+Math.E;
+Math.exp(3);     // Math.E cubed
+Math.sqrt(3);    // The square root of 3
+Math.pow(3,1/3); // The cube root of 3
