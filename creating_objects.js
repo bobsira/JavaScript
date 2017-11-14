5cca562c634c77547813deba663132634546183a
bobsira <sbobfitz2@gmail.com>
2017-11-15 00:50:35 +0300

    Creating Objects : Object Literals

---
+++ creating_objects.js
@@ -0,0 +1,15 @@
+var empty = {};
+
+var point = { x:0, y:0};
+
+var point2 = {x:point.x, y:point.y+1};
+
+var book = {
+  "main title": "JavaScript",
+  'sub-title': "The Definitive Guide",
+  "for": "all audiences",
+  author: {
+    firstname:"David",
+    surname:"Flanagan"
+  }
+};
