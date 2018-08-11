9e4961f949bf95a917c5e2ca30be00aaf9265d8b
bobsira <sbobfitz2@gmail.com>
2018-08-11 21:56:52 +0300

    Schema types (fields)

--- nodestuggles/express-locallibrary-tutorial/mongoose.js
+++ nodestuggles/express-locallibrary-tutorial/mongoose.js
@@ -9,3 +9,16 @@ var SomeModelSchema = new Schema({
 });
 // Compile model from schema
 var SomeModel = mongoose.model('SomeModel',SomeModelSchema);
+
+var schema = new Schema({
+  name : String,
+  binary : Buffer,
+  living : Boolean,
+  updated : { type : Date, default : Date.now },
+  age : { type : Number, min : 18, max : 65, required : true },
+  mixed : Schema.Types.Mixed,
+  _someId : Schema.Types.ObjectID,
+  array: [],
+  ofString : [String], //You can also have an array of each of the other types too.
+  nested: { stuff : { type: String, lowercase : true, trim : true }}
+})
