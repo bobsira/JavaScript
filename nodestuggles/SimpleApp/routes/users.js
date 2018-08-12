6877d0d1079451550971ae3f03ef22311f2b772d
bobsira <sbobfitz2@gmail.com>
2018-08-12 16:30:50 +0300

    Updates on the Project given

--- nodestuggles/SimpleApp/routes/users.js
+++ nodestuggles/SimpleApp/routes/users.js
@@ -1,53 +1,39 @@
 var express = require('express');
+var Request = require("request");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  postToApi();
-  res.send('respond with a resource');
-});
-
-
-/* GET json data from post request . */
-router.get('/data', function(req, res) {
-  var db = req.db;
-  var collection = db.get('data');
-  collection.find({},{},function(e,docs){
-    res.json(docs);
-  });
-});
-
-
-
-function postToApi(){
+var results = [];
+function postToApi() {
   Request.post({
-    "headers": { "content-type": "application/json" },
+    "headers":{ "content-type": "application/json" },
     "url": "https://api.thingspeak.com/update.json",
     "body": JSON.stringify({
       "api_key": "j3GER6CSBNU6PIIZ",
       "field1": "123"
     })
-  }, (error, response, body) => {
-    if(error) {
-      return console.dir(error);
-    }
-
-    var data = JSON.parse(body);
-    console.dir(JSON.parse(body));
-    console.log(data);
-    //insert data to my database
-    var db = req.db;
-    var collection = db.get('data');
-    collection.insert(data, function(err, result){
-      res.send(
-        (err === null) ? { msg: 'Successful' } : { msg: err }
-      );
-    });
-
+  }, (error,response,body) =>{
+    if(error) {return console.dir(error);}
+    results = JSON.parse(body);
   });
-
 }
 
+postToApi();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/*test two routes*/
+// router.get('/test',function (req,res,next) {
+//   res.send(results);
+// })
+
+/* GET test page. */
+router.get('/test', function(req, res, next) {
+  console.log(results);
+  res.render('test', { result: results,title: 'Test'});
+});
 
 
 
